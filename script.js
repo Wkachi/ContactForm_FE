3a6e96cd751444c66ffdaf91c741b42c45e7980d
Kachi G. Wachikwu <123011868+Wkachi@users.noreply.github.com>
2023-09-02 09:47:40 +0000

    updated with JS codes

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("testing")
+
+const biodataForm = document.querySelector(".BiodataForm");
+const submitButton = document.querySelector(".submit");
+
+submitButton.addEventListener("click", () =>{
+    biodataForm.style.display = "none";
+});
+
+const fName = document.querySelector("#fName").addEventListener("click", (e) =>{
+    //console.log(e);
+     e.enterKeyHint == " " || e.keyCode == " "  
+        console.log("Working")
+        biodataForm.style.display = "none";
+     
+});
